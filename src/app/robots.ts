cedd9d016fd8992c682ad82eac22dbff298f3221
Talha Shams <talhashamsch@gmail.com>
2026-03-12 22:27:32 +0500

    commit

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,12 @@ export default function robots(): MetadataRoute.Robots {
         rules: {
             userAgent: '*',
             allow: '/',
-            disallow: '/admin/',
+            disallow: [
+                '/admin/',
+                '/projects',
+                '/cdn-cgi/l/email-protection',
+                '/products/ht-switchgear-panels'
+            ],
         },
         sitemap: 'https://www.egswitchgear.com/sitemap.xml',
     }
